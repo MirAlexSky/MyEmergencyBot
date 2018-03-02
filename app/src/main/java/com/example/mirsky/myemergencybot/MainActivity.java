71439b7e68c3eb4a1d375d1ad24c80322f640878
MIRsky <miralexsky@gmail.ru>
2018-03-02 20:54:53 +0400

    Begin, day1, 1.0

--- app/src/main/java/com/example/mirsky/myemergencybot/MainActivity.java
+++ app/src/main/java/com/example/mirsky/myemergencybot/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.mirsky.myemergencybot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final double APP_VERSION = 1.0;
+
+    TextView txtV;
+    Button btnHelp;
+    ImageButton iBtnSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHelp = findViewById(R.id.btnHelp);
+        iBtnSet = findViewById(R.id.iBtnSet);
+        txtV = findViewById(R.id.txtV);
+
+        txtV.setText(String.valueOf(APP_VERSION));
+
+        btnHelp.setOnClickListener(new btnClickListener());
+        iBtnSet.setOnClickListener(new btnClickListener());
     }
+
+    class btnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.btnHelp:
+                    justMessage("Эта часть приложения ещё дорабатывается");
+                    break;
+                case R.id.iBtnSet:
+                    Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    }
+
+    void justMessage(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/mirsky/myemergencybot/SettingsActivity.java
@@ -0,0 +1,61 @@
+package com.example.mirsky.myemergencybot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    ImageButton iBtnBack;
+    TextView txtContacts, txtMessage, txtButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        iBtnBack = findViewById(R.id.iBtnBack);
+
+        txtContacts = findViewById(R.id.txtContacts);
+        txtMessage = findViewById(R.id.txtMessage);
+        txtButton = findViewById(R.id.txtButton);
+
+        txtContacts.setOnClickListener(new txtClickListener());
+        txtMessage.setOnClickListener(new txtClickListener());
+        txtButton.setOnClickListener(new txtClickListener());
+
+        iBtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    class txtClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.txtContacts:
+                    justMessage("Эта часть приложения ещё доробатывается");
+                    break;
+                case R.id.txtMessage:
+                    justMessage("Эта часть приложения ещё доробатывается");
+                    break;
+                case R.id.txtButton:
+                    justMessage("Эта часть приложения ещё доробатывается");
+                    break;
+            }
+        }
+    }
+
+    void justMessage(String str) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+    }
+
+}
